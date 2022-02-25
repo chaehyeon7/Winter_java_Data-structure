b7385ccd4f2fafdf3b496ea6d37bb66a56280fad
chaehyeon <81363193+chaehyeon7@users.noreply.github.com>
2022-02-25 18:15:52 +0900

    리스트

---
+++ src/part/Exam1.java
@@ -0,0 +1,15 @@
+package part;
+
+public class Exam1 {
+	public static void main(String[] args) {
+		int[] numbers = {10,20,30,40,50};
+		LinkedList numbers = new LinkedList();
+		numbers.add(10);
+		numbers.add(20);
+		numbers.add(30);
+		numbers.add(40);
+		numbers.add(50);
+		numbers.remove(3);
+		System.out.println(numbers);
+	}
+}
