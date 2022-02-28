c88386dbd5bb4a28e7eb83da5dbcb7c523c00f11
chaehyeon <81363193+chaehyeon7@users.noreply.github.com>
2022-02-28 15:26:46 +0900

    데이터 추가
    중간위치에 추가

--- part3/ArrayList.java
+++ part3/ArrayList.java
@@ -11,5 +11,16 @@ public class ArrayList {
 		    size++;
 		    return true;
 		}
+	 public boolean add(int index, Object element) {
+		    // 엘리먼트 중간에 데이터를 추가하기 위해서는 끝의 엘리먼트부터 index의 노드까지 뒤로 한 칸씩 이동시켜야 합니다.
+		    for (int i = size - 1; i >= index; i--) {
+		        elementData[i + 1] = elementData[i];
+		    }
+		    // index에 노드를 추가합니다.
+		    elementData[index] = element;
+		    // 엘리먼트의 숫자를 1 증가시킵니다.
+		    size++;
+		    return true;
+		}
 }
 
