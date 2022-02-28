f45bc7d70ba9dde637b598a0c01df72f070b2f72
chaehyeon <81363193+chaehyeon7@users.noreply.github.com>
2022-02-28 15:10:28 +0900

    객체 생성

---
+++ part3/ArrayList.java
@@ -0,0 +1,8 @@
+package part3;
+
+//ArrayList.javapackage list.arraylist.implementation;
+
+public class ArrayList {
+ private int size = 0;
+ private Object[] elementData = new Object[100];
+}
---
+++ part3/Main.java
@@ -0,0 +1,11 @@
+package part3;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList numbers = new ArrayList();
+
+    }
+
+}
