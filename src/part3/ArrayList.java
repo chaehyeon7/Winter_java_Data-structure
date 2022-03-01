b34024a564e628bb1191e7b35b53c014d6e46ac6
chaehyeon <81363193+chaehyeon7@users.noreply.github.com>
2022-03-01 16:15:49 +0900

    데이터 가져오기

---
+++ src/part3/ArrayList.java
@@ -0,0 +1,33 @@
+package part3;
+
+//ArrayList.javapackage list.arraylist.implementation;
+
+public class ArrayList {
+	 private int size = 0;
+	 private Object[] elementData = new Object[100];
+	 
+	 public boolean addLast(Object element) {
+		    elementData[size] = element;
+		    size++;
+		    return true;
+		}
+	 public boolean addFirst(Object element) {
+		 return add(0,element);
+	 }
+	 public boolean add(int index, Object element) {
+		    // 엘리먼트 중간에 데이터를 추가하기 위해서는 끝의 엘리먼트부터 index의 노드까지 뒤로 한 칸씩 이동시켜야 합니다.
+		    for (int i = size - 1; i >= index; i--) {
+		        elementData[i + 1] = elementData[i];
+		    }
+		    // index에 노드를 추가합니다.
+		    elementData[index] = element;
+		    // 엘리먼트의 숫자를 1 증가시킵니다.
+		    size++;
+		    return true;
+		}
+	public Object get(int index) {
+		return elementData[index];
+	}
+	 
+}
+
---
+++ src/part4/Main.java
@@ -0,0 +1,19 @@
+package part4;
+
+public class Main {
+
+	public static void main(String[] args) {
+	    ArrayList numbers = new ArrayList();
+	    numbers.addLast(10);
+	    numbers.addLast(20);
+	    numbers.addLast(30);
+	    numbers.addLast(40);
+	    
+	    System.out.println(numbers.get(0));
+	    System.out.println(numbers.get(1));
+	    System.out.println(numbers.get(2));
+	    
+
+	}
+
+}
